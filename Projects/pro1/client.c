717adfd27a80b8f4a337c7dd4c9ab4bafae450ff
William George <georgew@flip3.engr.oregonstate.edu>
2016-10-24 21:52:04 -0700

    started c file

--- Projects/pro1/client.c
+++ Projects/pro1/client.c
@@ -8,20 +8,25 @@
 
 int main(int argc, char *argv[])
 {
-	int a_portNum;
+	long a_portNum;
 	char *a_hostName;
+	char *eptr;
+	char handle[11];
 
 	if(argc != 3){
 		fprintf(stderr, "client <HOST A NAME> <HOST A PORT>\n");
 		exit(1);
 	}
 
-	printf("The port number is %s\n", argv[3]);
-	printf("The host name is %s\n", argv[2]);
+	printf("The port number is %s\n", argv[2]);
+	printf("The host name is %s\n", argv[1]);
 
+	a_portNum = strtol(argv[2], &eptr, 10);
+	printf("Port Number %ld\n", a_portNum); 
 
-
-
+	printf("Please Enter A Handle: ");
+	scanf("%s", handle);
+	printf("You chose %s\n", handle);
 
 
 	return 0;
