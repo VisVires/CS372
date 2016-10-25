e2c0b5470ce494f7e5dd65ed45d8daed6b2814dd
Will George <wgeorge@outlook.com>
2016-10-24 21:27:38 -0700

    added some c file

--- Projects/pro1/client.c
+++ Projects/pro1/client.c
@@ -3,14 +3,27 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char *argv[])
+{
+	int a_portNum;
+	char *a_hostName;
+
+	if(argc != 3){
+		fprintf(stderr, "client <HOST A NAME> <HOST A PORT>\n");
+		exit(1);
+	}
+
+	printf("The port number is %s\n", argv[3]);
+	printf("The host name is %s\n", argv[2]);
 
 
 
 
 
 
-return 0;
+	return 0;
 
 }
