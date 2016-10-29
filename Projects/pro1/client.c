7076845517f058b492ba930691c640aa3baf78d9
William George <georgew@flip3.engr.oregonstate.edu>
2016-10-29 15:41:56 -0700

    finshed pro1

--- Projects/pro1/client.c
+++ Projects/pro1/client.c
@@ -5,30 +5,140 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+#define MAXDATASIZE 500 	//max data size is 500 bytes
 
 int main(int argc, char *argv[])
 {
-	long a_portNum;
-	char *a_hostName;
-	char *eptr;
-	char handle[11];
+	int sockfd, numbytes, status, read_result, write_result;
+	struct addrinfo addr, *res;
+	char *hostname;
+	char *port;
+	size_t handleSize = 11;
+	char *handle;
+	char *a_handle;
+	char rbuffer[MAXDATASIZE+1];
+	char wbuffer[MAXDATASIZE+1];
+	char quit[] = "\\quit";
 
+	//make sure user input is correct
 	if(argc != 3){
-		fprintf(stderr, "client <HOST A NAME> <HOST A PORT>\n");
+		fprintf(stderr, "usage: client <HOST A NAME> <HOST A PORT>\n");
 		exit(1);
 	}
+	
+	//get commandline args
+	hostname = argv[1];
+	port = argv[2];
 
-	printf("The port number is %s\n", argv[2]);
-	printf("The host name is %s\n", argv[1]);
+	//reset hints to 0
+	memset(&addr, 0, sizeof(addr));
+	addr.ai_family = AF_UNSPEC;		//don't care if IPv4 or IPv6
+	addr.ai_socktype = SOCK_STREAM;
+	
+	getaddrinfo(hostname, port, &addr, &res);	
+	
+	//make socket
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sockfd < 0)
+	{
+		printf("error creating socket\n");
+		exit(1);
+	}
+	//printf("Socket Created\n");
 
-	a_portNum = strtol(argv[2], &eptr, 10);
-	printf("Port Number %ld\n", a_portNum); 
+	//connect to server
+	status = connect(sockfd, res->ai_addr, res->ai_addrlen);
+	if (status < 0){
+		printf("Error connecting to server\n");
+		exit(1);
+	}
+	printf("Connected\n");
 
-	printf("Please Enter A Handle: ");
-	scanf("%s", handle);
-	printf("You chose %s\n", handle);
 
-
+	//choose and send client handle
+	printf("Please Enter A Handle ");
+	handle = malloc(sizeof(char) * 11);
+	int hlength = 0;
+	//get appropriate length handle
+	while(hlength == 0 || hlength > 11){
+		printf("10 Characters Long: ");
+		hlength = getline(&handle, &handleSize, stdin);
+	}
+	//fflush(stdout);
+	handle[strcspn(handle, "\n")] = 0;
+	//printf("handle: %s\n", handle);
+	//send to user
+	write_result = send(sockfd, handle, 11, 0);
+	if (write_result == -1){
+		printf("Write Error");
+		exit(1);
+	}
+	
+	
+	//get handle from server
+	a_handle = malloc(sizeof(char) * 11);
+	read_result = recv(sockfd, a_handle, handleSize, 0);
+	if (read_result == -1){
+		printf("Read Error");
+		exit(1);
+	}
+	//}
+	//add null character to read input
+	a_handle[read_result] = '\0';
+	printf("You are now speaking to: %s\n", a_handle);
+	printf("You Start!>");
+	//flush stdout
+	fflush(stdout);
+	
+	//continue until connection is closed
+	while(1)
+	{
+			
+		//reset write buffer	
+		memset(&wbuffer, 0, sizeof(wbuffer));	
+		
+		//send response
+		fflush(stdout);
+		printf(">");
+		fgets(wbuffer, MAXDATASIZE+1, stdin);		
+		//check for quit signal
+		if (strncmp(wbuffer, "\\quit", 5) == 0){
+			printf("Goodbye\n");
+			close(sockfd);
+			exit(0);
+		}
+		//write message to server
+		write_result = send(sockfd, wbuffer, strlen(wbuffer), 0);
+		if (write_result == -1){
+			printf("Write Error");
+			close(sockfd);
+			exit(1);
+		}
+		
+				
+		//reset read buffer
+		memset(&rbuffer, 0, sizeof(rbuffer));
+		if(read_result = recv(sockfd,rbuffer, MAXDATASIZE, 0)){
+			if (read_result == -1){
+				printf("Read Error");
+				close(sockfd);
+				exit(1);
+			}
+			else{
+				rbuffer[read_result] = '\0';
+				//printf("%d", read_result);
+				printf("%s> %s\n", a_handle, rbuffer);
+			}
+		}
+		//flush standard input
+		fflush(stdout);
+	} 
+	//close client socket
+	close(sockfd);	
 	return 0;
-
 }
--- Projects/pro1/server.py
+++ Projects/pro1/server.py
@@ -3,50 +3,88 @@
 from socket import *
 import sys
 
+MAXDATASIZE = 500
+
+#https://docs.python.org/3.3/howto/sockets.html
+
+#send msg function
+def sendmsg(msg, sock, addr):
+	totalsent = 0
+	while totalsent < len(msg):
+		sent = sock.send(msg[totalsent:])
+		if sent == 0:
+			print "Connection with {} is closed".format(addr)
+			sock.close()
+		totalsent = totalsent + sent
+
+#recieve message fxn
+def recmsg(sock, size):
+	msg = ''
+	msg = sock.recv(size)
+	return msg 
+
 
 def main(argv):
 
 	#check if there is the correct number of arguments in the command line
 	if len(sys.argv) < 2:
-		print 'server.py <PORTNUMBER>'
+		print 'Usage: server.py <PORTNUMBER>'
 		sys.exit(2)
 	elif len(sys.argv) > 2:
-		print 'server.py <PORTNUMBER>'
+		print 'Usage: server.py <PORTNUMBER>'
 		sys.exit(2)
 
-	port= sys.argv[1]
-	print 'port number is', port 
+	#get port from command line args
+	port= int(sys.argv[1])
 
-
-	#get server port from user
-	serverPort = int(port)
-	
+	handle = ''	
 	#get user handle
-	handle = raw_input('Input User Handle: ')
-
-	#print "User handle is: %s" %handle
+	while len(handle) == 0 or len(handle) > 10:
+		handle = raw_input('Input 10 Character Server Handle: ')
 	
 	# create listening TCP socket
 	serverSocket = socket(AF_INET, SOCK_STREAM)
-	serverSocket.bind(('', serverPort))
+	serverSocket.bind(('', port))
 	
-
 	# listen for incoming TCP requests
 	serverSocket.listen(1)
-	print 'The Server is ready to recieve'
+	print 'The Server is ready to receive'
 	#wait for accept() on incoming requests
 	
-	sys.exit()
 	while 1:
 		connectionSocket, addr = serverSocket.accept()
 		#read bytes from socket
-		sentence = connectionSocket.recv(1024)
-		capitalizedSenntence = sentence.upper()
-		connectionSocket.send(capitalizedSentence)
-		#close connection
+		print "connection made from address {}".format(addr)
+		#get handle
+		
+		b_handle = recmsg(connectionSocket, 1024)
+		#strip trailing new line
+		b_hand = b_handle.rstrip('\n')
+		print "You are speaking to: " + b_hand
+			
+		#prompt for recieved messages
+		recprompt = b_hand + ">"
+				
+		#send handle to client
+		connectionSocket.send(handle)
+
+		while 1:
+			#receive message
+			recmessage = connectionSocket.recv(MAXDATASIZE)
+			if recmessage:
+				recmessage = recmessage.rstrip()
+				print recprompt + recmessage	
+			
+			#send message
+			sentmessage = raw_input(">")
+			if sentmessage == '\quit':
+				print "Goodbye"
+				break
+			sendmsg(sentmessage, connectionSocket, addr)
+			
+			#close connection
 		connectionSocket.close()
-
-
+		print "Connection with {} is closed".format(addr)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
