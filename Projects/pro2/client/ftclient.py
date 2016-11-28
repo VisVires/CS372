a47d4dd1f7c9c9a026e0250ba828955718cadd27
William George <georgew@flip1.engr.oregonstate.edu>
2016-11-27 22:46:33 -0800

    finished project

--- Projects/pro2/client/ftclient.py
+++ Projects/pro2/client/ftclient.py
@@ -1,43 +1,133 @@
-#!usr/bin/python
+#!/usr/bin/env python
 
 from socket import *
 import sys
+import os
 
 MAXSIZE = 2048
 
+
+#send message to server
 def sendMsg(sock, data):
+	#print data
 	sock.sendall(data)
 
-
+#receive data from server
 def recvMsg(sock, size):
-	buffer = []
+	buf = ''
 	totalRead = 0
+	#print size
 	if size == MAXSIZE:
 		data = sock.recv(MAXSIZE)
 		return data
-	while totalRead < size:
-		data = sock.recv(min(size-totalRead, MAXSIZE))
-		if data == b'':
-			print "Error"
-		buffer.append(data)
+	while totalRead + 1 < size:
+		data = sock.recv(MAXSIZE)
+		#print data
 		totalRead = totalRead + len(data)
-	return b''.join(data)
+		#print totalRead + 1
+		buf = buf + data	
+	return buf
 
+
+#print directory recieved from server then close socket
 def recvDir(sock):
 	direct = recvMsg(sock, MAXSIZE)
 	print direct
-	
+	sock.close()
 
-def recvfile(sock, filename):
+	
+#receive file from server and save to directory then close socket
+#stack overflow question 678236
+def recvfile(sock, filename, host, dPort):
+	#output message
+	ofilename = filename
+	num = 0
+	filenum = ''
+	filename = os.path.splitext(filename)[0]
+	#handle file exists error
+	while os.path.isfile('./' + filename + '.txt'):
+		#print "exists"
+		num = num + 1
+		filenum = str(num)
+		filename = filename + filenum
+	filename = filename + '.txt'
+	#print filename
 	outfile = open(filename, 'w+')
-	#size = recvMsg(sock, MAXSIZE)
+	size = recvMsg(sock, MAXSIZE)
 	#print repr(size) + '\n'
-	#sendMsg(sock, "OK")
-	data = recvMsg(sock, MAXSIZE)
+	size = size.rstrip('\0')
+	size = int(size)
+	sendMsg(sock, "OK")
+	print "Receiving \"{0}\" from {1}: {2}".format(ofilename, host, dPort)
+	data = recvMsg(sock, size + 1);
 	#print data
 	outfile.write(data)
 	outfile.close()	
+	sock.close()
 	print "File transfer complete"
+
+
+# make initial connection with server
+def initiateContact(port, host, dPort, command):
+	#cast port
+	port = int(port)
+	#change host to proper set up
+	hostname = host + ".engr.oregonstate.edu"
+	#make connection with waiting server
+	sock = socket(AF_INET, SOCK_STREAM)
+	sock.connect((hostname, port))
+	#send data port to server	
+	sendMsg(sock, dPort)
+	#get confimation	
+	validPort = recvMsg(sock, MAXSIZE)
+	if validPort == "OK":
+		#send command to server
+		sendMsg(sock, command)	
+		#get confirmation	
+		com = recvMsg(sock, MAXSIZE)
+		#check if valid
+		if com == "OK":
+			#return sock
+			return sock
+		elif com == "FAIL":
+			#
+			print "Error: Invalid Command from {0}:{1}".format(host, port)
+			sock.close()
+			sys.exit(2)
+
+	
+#check if file is valid and get directory
+def makeRequest(sock, dsock, command, host, port, dPort, filename):
+	if command == "-g":
+		#send file name
+		sendMsg(dsock, filename)
+		#get error from original socket
+		validfile = recvMsg(dsock, MAXSIZE)
+		#get confirmation
+		if validfile == "OK":
+			recvfile(dsock, filename, host, dPort)
+			dsock.close()
+		elif validfile == "FAIL":
+			dsock.close()
+			return -1
+	elif command == "-l":
+		print "Receiving directory structure from {0}:{1}".format(host, dPort)
+		directory = recvDir(dsock)
+		dsock.close()
+
+#check if command line command is valid
+def validateCommand(command):
+	if command != '-l' and command != '-g':
+		print "Error: Invalid Command"
+		sys.exit(2) 
+
+
+def validatePort(number):
+	try:
+		num = int(number)
+	except ValueError:
+		print("Use a Valid Port")
+		sys.exit(2)
 
 
 def main(argv):
@@ -46,67 +136,51 @@ def main(argv):
 		print "USAGE: <SERVER HOST> <SERVER PORT> <COMMAND> [FILENAME] <DATA PORT>"
 		sys.exit(2)		
 	
-	host = sys.argv[1] + ".engr.oregonstate.edu"
+	host = sys.argv[1] 	
 	port = sys.argv[2]
 	command = sys.argv[3]
 
 	if len(sys.argv) == 5:
+		filename = None
 		dPort = sys.argv[4]	
 	elif len(sys.argv) == 6: 
 		filename = sys.argv[4]
 		dPort = sys.argv[5]
 		#print filename
 
-	#print host
-	#print port
-	#print command
-	#print dPort
-	#if filename:
-	#	print filename
+	#validate port and command inputs
+	validatePort(port)
+	validatePort(dPort)
+	validateCommand(command);
 
-	port = int(port)
-	sock = socket(AF_INET, SOCK_STREAM)
-	sock.connect((host, port))
-	#send data port to server
-	sendMsg(sock, dPort)
-	#check if it worked
-	validPort = recvMsg(sock, MAXSIZE)
-	if validPort == "OK":
-		#send selected command to server
-		sendMsg(sock, command)
-		com = recvMsg(sock, MAXSIZE)
-		#verify command
-		if com == "OK":
-			#print "VALID COMMAND"
-			#open new server
-			dsock = socket(AF_INET, SOCK_STREAM)
-			dPort = int(dPort)
-			dsock.connect((host, dPort))
-			#dsock.setblocking(0)
-			#get file
-			if command == "-g":
-				#send file name
-				sendMsg(dsock, filename)
-				#get error from original socket
-				validfile = recvMsg(dsock, MAXSIZE)
-				#get confirmation
-				if validfile == "OK":
-					recvfile(dsock, filename)
-				elif validfile == "FAIL":
-					print "{0}:{1} says FILE NOT FOUND".format(sys.argv[1], port)
-					dsock.close()
-					sock.close()
-					sys.exit(2)
-			elif command == "-l":
-				print "Receiving directory structure from {0}:{1}".format(sys.argv[1], dPort)
-				directory = recvDir(dsock)
-			dsock.close()
-		elif com == "FAIL":
-			print "INVALID COMMAND"
-			sock.close()
-			sys.exit(2)
+	#make inital connection
+	sock = initiateContact(port, host, dPort, command)
+	
+	#make data connection	
+	dsock = socket(AF_INET, SOCK_STREAM)
+	dPort = int(dPort)
+	
+	dsock.bind(('', dPort))
+	dsock.listen(1)
+	#print "Data Connection open"
+	
+	newsock, addr = dsock.accept()
+	#print "connection made from address: {0}".format(addr)
+	'''
+	dsock.connect((host, dPort))
+	#dsock.setblocking(0)
+	'''
+	#get file
+	valid = makeRequest(sock, newsock, command, host, port, dPort, filename)
+	if valid == -1:
+		print "{0}:{1} says FILE NOT FOUND".format(sys.argv[1], port)
+		dsock.close()
+		sock.close()
+		sys.exit(2)
+	
+	newsock.close()
+	dsock.close()
 	sock.close()
-
 
 
 if __name__ == "__main__":
--- Projects/pro2/ftserver.c
+++ Projects/pro2/ftserver.c
@@ -1,3 +1,18 @@
+/*
+ *	Name: William George
+ *	Project 2
+ *	Files: ftserver.c, ftclient.py, README, Makefile
+ *
+ *	Purpose: Create ftp server
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
@@ -13,14 +28,16 @@
 
 #define MAXSIZE 1024 
 
-void dataConnection(char *, int, int);
+
+void dataConnect(char *dPort, int com, char *hostname, int sock, char *oport);
 void sendDirContents(int, char *, char *);
 void sendFileToClient(int, char*, char *, char *);
 void readClient(int, char *, int);
 void writeClient(int, char *, int);
 int fileExist(char *);
-
-
+void validPort(char *);
+//int startup(char *);
+void makeRequest(int sock, int command, char *dPort, char *hostname, char *oport);
 
 int main(int argc, char *argv[])
 {
@@ -30,20 +47,21 @@ int main(int argc, char *argv[])
 	char *get = "-g";
 	char *good = "OK";
 	char *badName = "FAIL";	
-	char ipstr[INET6_ADDRSTRLEN];
+	//char ipstr[INET6_ADDRSTRLEN];
 	int sockfd, new_socket, client_port;;
 	struct sockaddr_storage cli_addr;
 	socklen_t addr_size;
 	struct addrinfo serv_addr, *res;
 	int read_result, write_result;	
-
+	
 	if (argc != 2) {
 		fprintf(stderr, "Usage: ftserver <SERVER_PORT>\n");
 		exit(1);
 	}
 	//set port number
 	port = argv[1];
-	
+	//startup (port);
+	validPort(port);
 	//clear memory for serv_addr and set up serv_addr	
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.ai_family = AF_UNSPEC;
@@ -68,7 +86,8 @@ int main(int argc, char *argv[])
 	if(bind(sockfd, res->ai_addr, res->ai_addrlen) == -1){
 		error("Bind 1 Failed\n");
 	};	
-
+	
+	//sockfd = startup(port);
 	//listen for new connections
 	listen(sockfd, 10);
 	printf("Server open on: %s\n", port);	
@@ -89,17 +108,13 @@ int main(int argc, char *argv[])
 		
 		int com;
 		int validCommand = 1;
-		//printf("Connected\n");
-			
-		//output client details
-		//beej.us/guide/output/html/multipage/getpeernameman.html
-		getpeername(new_socket, (struct sockaddr *) &cli_addr, &addr_size);
-		struct sockaddr_in *o_sock = (struct sockaddr_in *) &cli_addr;
-		client_port = ntohs(o_sock->sin_port);
-		inet_ntop(AF_INET, &o_sock->sin_addr, ipstr, sizeof ipstr);
-		printf("Connection from: %s at Client Port: %d\n", ipstr, client_port);
-		
+	
+		char hostname[MAXSIZE];
+		char service[MAXSIZE];  
+		getnameinfo((struct sockaddr *) &cli_addr, sizeof (struct sockaddr), hostname, MAXSIZE, service, MAXSIZE, 0);
+		printf("Connection made with %s\n", hostname);
 		//get port from client
+		
 		memset(&buffer, 0, MAXSIZE);
 		read_result = recv(new_socket, buffer, sizeof(buffer), 0);
 		if (read_result == -1){
@@ -121,7 +136,7 @@ int main(int argc, char *argv[])
 		memset(&command, 0, MAXSIZE);
 		read_result = recv(new_socket, command, sizeof(command), 0);	
 		if (read_result == -1){
-			error("Error getting new port");
+			error("Error getting command");
 		}
 		else{
 			command[read_result] = '\0';
@@ -129,7 +144,7 @@ int main(int argc, char *argv[])
 			
 		//printf("Command: %s\n", command);
 		//check if command valid and send confirmation or invalid statement
-		if(strcmp(command,list) != 0 && strcmp(command, get) != 0){
+		if(strcmp(command, list) != 0 && strcmp(command, get) != 0){
 			printf("Invalid Command\n");
 			validCommand = 0;
 			//send invalid name to client
@@ -137,6 +152,7 @@ int main(int argc, char *argv[])
 			close(new_socket);
 		}
 		else{
+			//set command to integers 
 			if(strcmp(command, list) == 0)
 			{
 				com = 1;
@@ -160,12 +176,14 @@ int main(int argc, char *argv[])
 			if(cpid == 0){
 				//printf("In Child\n");
 				//send to data connection function
-				dataConnection(dPort, com, sockfd);	
+				dataConnect(dPort, com, hostname, sockfd, port);
+				//dataConnection(dPort, com, sockfd);	
 				close(sockfd);
 				close(new_socket);
 				exit(0);
 			}
 		}
+		
 		close(new_socket);
 	}
 	close(sockfd);	
@@ -173,118 +191,123 @@ int main(int argc, char *argv[])
 }
 
 /*
- *
- *
- *
- *
+ *	Function Purpose: Make data connection with client and send directory or file
+ *	Input: data port, client command, client hostname, connection socket
+ *	Output: Calls functions to send data to client
  *
  *
  *
 */
 
-void dataConnection(char *port, int command, int osock){
-
-	int sock, nsock;
+void dataConnect(char *dPort, int command, char *hostname, int osock, char *oport){
+	struct addrinfo addr, *res;
 	char buf[MAXSIZE];
 	char *filename;
 	char *good = "OK";
 	char *badName = "FAIL";
-	int nport, validfile;
-	char ipstr[INET6_ADDRSTRLEN];
-	struct sockaddr_storage cli_addr;
-	socklen_t addr_size;
-	struct addrinfo serv_addr, *res;
-	int read_result, write_result;	
+	int sock, read_result, write_result, status, validfile;
 
-	//printf("Command2: %s\n", command);
+	memset(&addr, 0, sizeof(addr));
+	addr.ai_family = AF_UNSPEC;
+	addr.ai_socktype = SOCK_STREAM;
+	//addr.ai_flags = AI_PASSIVE;
 	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.ai_family = AF_UNSPEC;
-	serv_addr.ai_socktype = SOCK_STREAM;
-	serv_addr.ai_flags = AI_PASSIVE;
-
-	getaddrinfo(NULL, port, &serv_addr, &res);	
-
-	//set up TCP server
+	getaddrinfo(hostname, dPort, &addr, &res);
+	
+	//open new socket
 	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	if (sock < 0){
-		error("error opening data socket\n");
+		printf("Error creating socket");
 		exit(1);
 	}
+	//printf("Socket created\n");
+	//class message board
+	sleep(1);
 
-	//beej.us.setcokoptman
-	int optval = 1;	
-	if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval) == -1)
-		error("setsockopt");
-	
-
-	//set up bind
-	if(bind(sock, res->ai_addr, res->ai_addrlen) == -1){
-		error("Bind 2 Failed\n");
-	};	
-
-	//listen for new connections
-	listen(sock, 10);
-	
-	//get client size 
-	addr_size = sizeof cli_addr;
-	
-	//clear client
-	memset(&cli_addr, 0, sizeof(cli_addr));
-	
-	//printf("Awaiting Second Connection\n");
-	nsock = accept(sock, (struct sockaddr *) &cli_addr, &addr_size);
-	if(nsock < 0)
-	{
-		printf("Connection Failed!!\n");
-		return;
+	//connect to server
+	status = connect(sock, res->ai_addr, res->ai_addrlen);
+	if(status < 0){
+		printf("Error making data connection to client");
+		close(sock);
+		exit(1);
 	}
-		
-	//output client details
-	getpeername(nsock, (struct sockaddr *) &cli_addr, &addr_size);
-	struct sockaddr_in *o_sock = (struct sockaddr_in *) &cli_addr;
-	nport = ntohs(o_sock->sin_port);
-	inet_ntop(AF_INET, &o_sock->sin_addr, ipstr, sizeof ipstr);
-	//printf("Connection from: %s at Client Port: %d\n", ipstr, nport);
 	
-	//if command is a list
+	makeRequest(sock, command, dPort, hostname, oport);		
+}
+
+/*
+ *	Function Purpose: Calls functions to send data to clients over data connection 
+ *	Input: data socket, command, data port, host name, old port
+ *	Output: Calls sendfile or senddir functions
+ *
+ */
+
+
+void makeRequest(int sock, int command, char *dPort, char *hostname, char *oport){
+
+	int read_result, write_result, validfile;
+	char buf[MAXSIZE];
+	char *filename;
+	char *good = "OK";
+	char *badName = "FAIL";
+	
+	//get commanded info
 	if (command == 1){
-		printf("List directory requested on port %s\n", port);
-		sendDirContents(nsock, port, ipstr);
+		printf("List directory requested on port %s\n", dPort);
+		sendDirContents(sock, dPort, hostname);
 	}
+	
 	//if command is a get
 	else if (command == 2){
 	
 		memset(&buf, 0, MAXSIZE);
 		//get filename from client	
-		read_result = recv(nsock, buf, sizeof(buf), 0);
+		read_result = recv(sock, buf, sizeof(buf), 0);
 		if (read_result == -1){
 			error("Error getting filename");
 		}
 		else{
 			buf[read_result] = '\0';
 		}		
-		printf("File \"%s\" requested on port %s\n", buf, port);
+		printf("File \"%s\" requested on port %s\n", buf, dPort);
 		validfile = fileExist(buf);
 		if (validfile){
 			//send confirmation
-			writeClient(nsock, good, strlen(good));
+			writeClient(sock, good, strlen(good));
 			//send file to client
-			sendFileToClient(nsock, buf, port, ipstr);
+			sendFileToClient(sock, buf, dPort, hostname);
 		}
 		else{
-			printf("File not found. Sending error message to %s: %s\n",ipstr ,port);
-			writeClient(nsock, badName, strlen(badName));
+			printf("File not found. Sending error message to %s: %s\n",hostname ,oport);
+			writeClient(sock, badName, strlen(badName));
 		}
-	}	
-		
-	close(nsock);
-	close(sock);
+	}		
 }
-
 /*
  *
+ *	Function  Purpose: Check if user port is valid number
+ *	Input: Str of port valie
+ *	Output: Exit if invalid
  *
+ */
+void validPort(char *port){
+	int length, i;
+	length = strlen(port);
+	for (i = 0; i < length; i++){
+		if (!isdigit(port[i])){
+			printf("Invalid Port\n");
+			exit(1);
+		}
+	}
+}
+
+
+
+
+/*
+ *	Function Purpose: Check if file exists in current directory
+ *	Input: filename
+ *	Output:return boolean to file contents
  *
  *
  */
@@ -300,13 +323,15 @@ int fileExist(char *filename){
 }
 
 /*
- *
- *
+ *	Function Purpose: Send file contents to client over data port
+ *	Input: data socket, filename, Client port, Client Hostname
+ *	Output: Sends file contents to client
  *
  */
-void sendFileToClient(int sock, char *filename, char *port, char *ip){
+void sendFileToClient(int sock, char *filename, char *port, char *hostname){
 	
 	char *fileContents = NULL;
+	int read_result;
 	char buf[MAXSIZE] = "";
 	char good[MAXSIZE];
 	size_t fileSize = 0;
@@ -327,20 +352,37 @@ void sendFileToClient(int sock, char *filename, char *port, char *ip){
 	//printf("%s\n", buf);
 	//printf("%s\n", fileContents);
 	//send size
-	//writeClient(sock, buf, sizeof(int));
+	writeClient(sock, buf, sizeof(buf));
 	//get confirm
-	//memset(&good, 0, MAXSIZE);
-	//readClient(sock, good, sizeof(good)); 
+	memset(&good, 0, MAXSIZE);
+	read_result = recv(sock, good, sizeof(good), 0);
+	
+	if (read_result == -1){
+		error("Error getting new port");
+	}
+	else{
+		good[read_result] = '\0';
+	}
+	//printf("Good?: %s\n", good); 
 	//send file contents
-	printf("Sending \"%s\" to %s: %s\n", filename, ip, port);
+	printf("Sending \"%s\" to %s: %s\n", filename, hostname, port);
 	writeClient(sock, fileContents, fileSize);
 	//free memory
 	free(fileContents);
+	close(sock);
 }
 
+/*
+ *	Function Purpose: Send directory contents to client on socket
+ *	Input: Socket, Data Port, Client Hostname
+ *	Output: Sends directory contents to client
+ *
+ *	Source: Stackoverflow Question 612097 
+ *
+ */
 
 
-void sendDirContents(int sock, char *port, char *ip){
+void sendDirContents(int sock, char *port, char *hostname){
 	DIR *d;
 	struct dirent *dir;
 	int files;
@@ -357,36 +399,14 @@ void sendDirContents(int sock, char *port, char *ip){
 			}
 		}
 		
-		printf("Sending directory contents to %s: %s\n", ip, port);
+		printf("Sending directory contents to %s: %s\n", hostname, port);
 		writeClient(sock, buffer, strlen(buffer));	
 		
 	}
+	close(sock);
 	closedir(d);
 }
 
-
-
-/*
- *
- *
- *
- *
- *
- */
-
-void readClient(int sock, char* buf, int size){
-	int read_result;
-	int totalSize = size;
-	//get port from client
-	memset(&buf, 0, MAXSIZE);
-	read_result = recv(sock, buf, sizeof(buf), 0);
-	if (read_result == -1){
-		error("Error getting new port");
-	}
-	else{
-		buf[read_result] = '\0';
-	}
-}
 
 /*
  *
